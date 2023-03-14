825fefc7f07765e965fdec51d4b8c7b2ca4eee0d
Martin Iliev <xxsuperdreamxx@gmail.com>
2023-03-14 19:14:42 +0100

    Added few of the game examples in inactive form

--- nmt-frontend/src/router/index.js
+++ nmt-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GamesView from '../views/GamesView.vue'
+import GameView from '../views/GameView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/games',
       name: 'games',
       component: GamesView
+    },
+    {
+      path: '/game',
+      name: 'game',
+      component: GameView
     },
     // {
     //   path: '/about',
---
+++ nmt-frontend/src/views/GameView.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="min-h-screen">
+        <main class="mt-24 pb-8">
+            <div class="mx-auto w-full max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                <div class="grid grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8">
+                    <div class="grid grid-cols-1 gap-4 lg:col-span-2">
+                        <section aria-labelledby="section-1-title">
+                            <div class="overflow-hidden rounded-lg bg-slate-800/50 shadow">
+                                <div class="p-6">
+                                    <div
+                                         class="grid grid-cols-6 gap-4 font-mono text-white text-sm text-center font-bold leading-6">
+                                        <div
+                                             class="p-4 rounded-lg col-span-full sm:col-start-2 sm:col-span-4 row-span-2 shadow-none">
+                                            <p class="text-4xl font-bold tracking-tight sm:text-5xl mb-7">21 + 20 = 41</p>
+                                        </div>
+
+                                        <div
+                                             class="p-4 rounded-lg border border-indigo-900 shadow-sm shadow-indigo-900 col-start-1 col-end-3">
+                                            <p class="text-3xl font-bold tracking-tight sm:text-4xl">20</p>
+                                        </div>
+                                        <div
+                                             class="p-4 rounded-lg border border-indigo-900 shadow-sm shadow-indigo-900 col-end-7 col-span-2">
+                                            <p class="text-3xl font-bold tracking-tight sm:text-4xl">20</p>
+                                        </div>
+                                        <div
+                                             class="p-4 rounded-lg border border-indigo-900 shadow-sm shadow-indigo-900 col-start-3 col-end-5">
+                                            <p class="text-3xl font-bold tracking-tight sm:text-4xl">20</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+
+                    <div class="grid grid-cols-1 gap-4">
+                        <section aria-labelledby="section-2-title">
+                            <div class="overflow-hidden rounded-lg bg-slate-800/50 shadow">
+                                <div class="p-6">
+                                    <h1 class="text-center ">Game Stats</h1>
+                                    <p>Score: 123</p>
+                                    <p>Level: 123</p>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <main class="mt-24 pb-8">
+            <div class="mx-auto w-full max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                <div class="grid grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8">
+                    <div class="grid grid-cols-1 gap-4 lg:col-span-2">
+                        <section aria-labelledby="section-1-title">
+                            <div class="overflow-hidden rounded-lg bg-slate-800/50 shadow">
+                                <div class="p-6 container">
+                                    <div
+                                         class="grid grid-cols-5 gap-y-4 font-mono text-white text-sm text-center font-bold leading-6 bg-stripes-sky rounded-lg place-items-center">
+                                        <div class="p-4 rounded-lg shadow-lg bg-sky-500 col-start-3 mb-10">
+                                            <svg aria-hidden="true"
+                                                 class="w-10 h-10"></svg>
+                                        </div>
+                                        <button class="p-4 rounded-lg shadow-lg bg-sky-500 col-start-3">
+                                            <svg aria-hidden="true"
+                                                 class="w-10 h-10"></svg>
+                                        </button>
+                                        <button class="p-4 rounded-lg shadow-lg bg-indigo-500 col-start-2 place-self-end">
+                                            <svg aria-hidden="true"
+                                                 class="w-10 h-10"></svg>
+                                        </button>
+                                        <button class="p-4 rounded-lg shadow-lg bg-fuchsia-500 col-start-4 place-self-start">
+                                            <svg aria-hidden="true"
+                                                 class="w-10 h-10"></svg>
+                                        </button>
+                                        <button class="p-4 rounded-lg shadow-lg bg-rose-500 col-start-3">
+                                            <svg aria-hidden="true"
+                                                 class="w-10 h-10"></svg>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+
+                    <div class="grid grid-cols-1 gap-4">
+                        <section aria-labelledby="section-2-title">
+                            <div class="overflow-hidden rounded-lg bg-slate-800/50 shadow">
+                                <div class="p-6">
+                                    <h1 class="text-center ">Game Stats</h1>
+                                    <p>Score: 123</p>
+                                    <p>Level: 123</p>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</template>
+
