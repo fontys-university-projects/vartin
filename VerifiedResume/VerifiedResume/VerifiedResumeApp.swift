c90f43768e6ed081f0db166e63b51197212928e1
i425590 <425>
2023-05-10 13:00:30 +0200

    Adding VerifiedResume initial commit

---
+++ VerifiedResume/VerifiedResume/VerifiedResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerifiedResumeApp.swift
+//  VerifiedResume
+//
+//  Created by Valentin on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct VerifiedResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
